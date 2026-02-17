6f9388c4e8e70e902b50fa74bd775e60a433fb7a
pierrejgb <145400668+pierrejgb@users.noreply.github.com>
2026-02-17 11:04:46 -0600

    Added LibraryH3lp

--- 01UWI_GB-GBNDE/assets/js/custom.js
+++ 01UWI_GB-GBNDE/assets/js/custom.js
@@ -1,1 +1,10 @@
-console.log('Helo from custom js');
+/*----------Code for LibraryH3lp chat widget-----------*/
+var needsJs = document.createElement('div');
+needsJs.setAttribute('class', 'needs-js');
+document.body.appendChild(needsJs);
+
+var s = document.createElement('script');
+s.id = 'localScript';
+s.src = 'https://libraryh3lp.com/js/libraryh3lp.js?19803';
+document.body.appendChild(s);
+/*---------------LibraryH3lp code ends here---------------*/
