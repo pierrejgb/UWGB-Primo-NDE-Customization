03feb2799bfb04d8a22adfb6e19f869ee8422f1c
pierrejgb <145400668+pierrejgb@users.noreply.github.com>
2026-04-01 16:14:38 -0500

    Matomo Tag Manager

--- 01UWI_GB-GBNDE/assets/js/custom.js
+++ 01UWI_GB-GBNDE/assets/js/custom.js
@@ -8,3 +8,12 @@ s.id = 'localScript';
 s.src = 'https://libraryh3lp.com/js/libraryh3lp.js?19803';
 document.body.appendChild(s);
 /*---------------LibraryH3lp code ends here---------------*/
+
+/*---------------Matomo Tag Manager---------------*/
+  var _mtm = window._mtm = window._mtm || [];
+_mtm.push({ 'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start' });
+(function () {
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true; g.src = 'https://cdn.matomo.cloud/uwgb.matomo.cloud/container_rXoVoDX6.js'; s.parentNode.insertBefore(g, s);
+})();
+/*---------------End Matomo Tag Manager---------------*/
